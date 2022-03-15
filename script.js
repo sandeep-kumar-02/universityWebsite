a8073153742fd37034a17c375ff88473cb3163a2
sandeep <ksandeepsharma02@gmail.com>
2022-03-16 05:21:19 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+    let el = document.querySelector('.nav-links');
+
+function showMenu(){
+    el.style.right = '0';
+}
+                                                       
+function hideMenu(){
+    el.style.right = '-200px';
+}
+
+
